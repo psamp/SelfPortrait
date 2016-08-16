b42571fa2819467a70cd14a9f53187bc2059dfee
Princess Sampson <ps@kingonomics.com>
2016-08-16 14:10:21 -0400

    Initial Commit

---
+++ SelfPortrait/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SelfPortrait
+//
+//  Created by Princess Sampson on 8/16/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
